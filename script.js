8fb3388cc9c0300954ee08e08744044c187ef003
Missaoui Mohamed Hosni <hosnimohamed1919@gmail.com>
2024-07-18 17:48:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ Array.from(buttons).forEach((button)=>{
   button.addEventListener("click",(e)=>{
      if( e.target.innerHTML == "="){
         string =eval(string);
-        document.querySelector("textarea").innerText = string;
+        document.querySelector("textarea").value = string;
      }
      else if(e.target.innerHTML=="AC"){
         string = "";
-        document.querySelector("textarea").innerText = string;
+        document.querySelector("textarea").value = string;
      }
      else if(e.target.innerHTML=="←"){
         string = string.slice(0,-1);
-        document.querySelector("textarea").innerText = string;
+        document.querySelector("textarea").value = string;
      }
      else {
         string = string + e.target.innerHTML;
-        document.querySelector("textarea").innerText = string;
+        document.querySelector("textarea").value = string;
      }
   })
 })
